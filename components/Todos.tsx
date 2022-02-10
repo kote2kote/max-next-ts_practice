413827c496b0c23e99dd50d7d4e11298601880ce
kote2 <kote2kote2@gmail.com>
2022-02-10 20:22:19 +0900

    409.models/todoでclass Todoを追加

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 // import { FC } from "react"
 
+import Todo from '../models/todo'; // class Todoの追加
+
 // type Todos
 // ----------------------------------------- //
 // React.FC(FunctionComoponents)で<{ items: string[] }>のジェネリクス型をreturnする。
 // <Todos items={["A", "B", "C"]} />のように記述してitems: string[]リストを受け取り
 // リスト表示する
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+// const Todos: React.FC<{ items: string[] }> = (props) => {
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  // class Todoの追加による変更
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        // <li key={item}>{item}</li> // class Todoの追加による変更
+        <li key={item.id}>
+          {item.text} | {item.id}
+        </li>
       ))}
     </ul>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 // import HelloWorld from '@components/hello-world';
 import Todos from '@components/Todos';
+import Todo from '../models/todo';
 const Index = () => {
+  const todos = [
+    new Todo('ラテくんかわいいね'),
+    new Todo('オレオくんかわいいね'),
+  ];
+
   return (
     <div className="bg-gray-200">
-      <Todos items={['ラテのオシッコ片付ける', 'オレオの吐瀉物を片付ける']} />
+      {/* <Todos items={['ラテのオシッコ片付ける', 'オレオの吐瀉物を片付ける']} /> */}
+      <Todos items={todos} />
     </div>
   );
 };
