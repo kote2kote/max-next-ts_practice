8607a67e3046e22f322725749be43dac83ed8d53
kote2 <kote2kote2@gmail.com>
2022-02-11 15:00:39 +0900

    414.Todoの追加(useState)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-// import HelloWorld from '@components/hello-world';
+import { useState } from 'react';
+
 import Todos from '@components/Todos';
 import NewTodo from '@components/NewTodo';
 import Todo from '../models/todo';
+
 const Index = () => {
-  const todos = [
-    new Todo('ラテくんかわいいね'),
-    new Todo('オレオくんかわいいね'),
-  ];
-  const addTodoHandler = (todoText: string) => {};
+  // const todos = [
+  //   new Todo('ラテくんかわいいね'),
+  //   new Todo('オレオくんかわいいね'),
+  // ];
+
+  // useState
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // 新規Todoを追加した時にpropsから呼び出される
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos(
+      // concatしたtodo
+      (prevTodos) => {
+        return prevTodos.concat(newTodo);
+      },
+    );
+  };
   return (
     <div className="bg-gray-200">
       {/* <Todos items={['ラテのオシッコ片付ける', 'オレオの吐瀉物を片付ける']} /> */}
